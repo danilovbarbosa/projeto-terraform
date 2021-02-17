96eba83b918da8d0dda8374496d42008e7ab366a
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-02-17 09:39:10 -0300

    Configurando DynamoDB.

--- region-us-east-2.tf
+++ region-us-east-2.tf
@@ -5,11 +5,31 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev6" {
-  ami = "ami-02fe94dee086c0c37"
+  provider = aws.us-east-2
+  ami = "ami-01aab85a5e4a5a0fe"
   instance_type = "t2.micro"
   key_name = "terraform-aws"
   tags = {
     Name = "dev6"
   }
   vpc_security_group_ids = [ aws_security_group.acesso_ssh_us_east_2.id, ]
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
 }
