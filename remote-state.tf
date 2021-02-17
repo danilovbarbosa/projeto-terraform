9579d287d0175c9ceb5d33db1f542a6cc85ff8e6
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-02-17 11:53:29 -0300

    Configurado o remote-state.tf para operar de forma remota.

---
+++ remote-state.tf
@@ -0,0 +1,11 @@
+# Using a single workspace:
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "danilovbarbosa"
+
+    workspaces {
+      name = "testando123"
+    }
+  }
+}
